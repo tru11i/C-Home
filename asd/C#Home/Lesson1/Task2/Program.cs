17c3d4aef7fc67e511fdcced7ca79115a9c58aee
Nuborak <mjumalievm@gmail.com>
2023-07-27 17:33:11 +0600

    commit

---
+++ asd/C#Home/Lesson1/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Vvedite chisla");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+if(A > B)
+
+{
+    Console.Write("Max = ");
+    Console.WriteLine(A);
+    Console.Write("Min = ");
+    Console.WriteLine(B);
+}
+else
+{
+    Console.Write("Max = ");
+    Console.WriteLine(B);
+    Console.Write("Min = ");
+    Console.WriteLine(A);
+}
