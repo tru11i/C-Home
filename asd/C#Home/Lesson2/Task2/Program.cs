b0e43676ca973ab421bb85c4bcaa7720fcf6c330
Nuborak <mjumalievm@gmail.com>
2023-07-27 21:09:46 +0600

    F

---
+++ asd/C#Home/Lesson2/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Vvedite chisla");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 999)
+{
+int fun(int num)
+{
+ while (num > 999)
+{
+  num = num / 10;
+}
+  return num;
+}
+ 
+}
+else if ((num < 999) && (num > 100))
+{
+  num = num % 10;
+  Console.WriteLine(num);
+}
+else
+{
+  Console.WriteLine("3 cifri net");
+}
