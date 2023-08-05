8753f665c8b98339d335d4de8f35fce6401f5dca
RICCI <74550924+tru11i@users.noreply.github.com>
2023-08-05 16:20:02 +0600

    Add files via upload

---
+++ asd/C#Home/Lesson3.cs
@@ -0,0 +1,48 @@
+﻿// Task1
+// Console.WriteLine("Vvedite chisla");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int num1 = num/10000;
+// int num2 = (num/1000)%10;
+// int num3 = (num/10)%10;
+// int num4 = num%10;
+
+// if((num1 == num4) && (num2 == num3))
+// {
+//   Console.WriteLine("Da");
+// }
+// else
+// {
+//   Console.WriteLine("Net");
+// }
+
+// Task2
+// Console.WriteLine("Vvedite chislo");
+// int X1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Vvedite chislo");
+// int Y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Vvedite chislo");
+// int Z1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Vvedite chislo");
+// int X2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Vvedite chislo");
+// int Y2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Vvedite chislo");
+// int Z2 = Convert.ToInt32(Console.ReadLine());
+
+// double num = Math.Sqrt(Math.Pow((X2-X1), 2) + Math.Pow((Y2-Y1), 2) + Math.Pow((Z2-Z1), 2));
+// Console.WriteLine(num);
+
+// Task3
+// Console.WriteLine("Vvedite chislo");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// int i = 1;
+
+// while(i <= N)
+// {
+//   double num = Math.Pow(i, 3);
+//   i++;
+//   Console.WriteLine(num);
+// }
