bf1ee05e977ebed26f6df2a75c9e1fdeb67b7472
RICCI <74550924+tru11i@users.noreply.github.com>
2023-08-06 14:34:16 +0600

    Add files via upload

---
+++ asd/C#Home/Lesson4.cs
@@ -0,0 +1,63 @@
+﻿// Task1.1
+
+// Console.WriteLine("Vvedite chislo");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Vvedite chislo");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// int Mult()
+// {
+//   int num = 1;
+//   for(int count = 1; count <= B; count++)
+//   {
+//     num = num * A;
+//   }
+//   return num;
+// }
+// Console.WriteLine(Mult());
+
+// Task1.2
+
+// Console.WriteLine("Vvedite chislo");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Vvedite chislo");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// double Function()
+// {
+//   double num = Math.Pow(A, B);
+//   return num;
+// }
+// double result = Function();
+// Console.WriteLine(result);
+
+// Task2
+
+// Console.WriteLine("Vvedite chislo");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int Function()
+// {
+//   int sum = 0;
+//   while(num > 0)
+//   {
+//     sum = sum + num%10;
+//     num/=10;
+//   }
+//   return sum;
+// }
+// Console.WriteLine(Function());
+
+//Task3
+
+// int [] array = new int[8];
+
+// void Get()
+// {
+//   for(int i = 0; i < array.Length; i++)
+//   {
+//     array[i] = new Random().Next(0,100);
+//     Console.Write(array[i] + " ");
+//   }
+// }
+// Get();
