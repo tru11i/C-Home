e4ea8a627262d4aa8075db274464cd2ebff86876
RICCI <74550924+tru11i@users.noreply.github.com>
2023-09-12 21:00:59 +0600

    Add files via upload

---
+++ asd/C#Home/Lesson6.cs
@@ -0,0 +1,44 @@
+﻿// Task1
+// Console.WriteLine("VVedite chislo cherez zapyatuu");
+// string input = Console.ReadLine();
+// string[] numStr = input.Split(',');
+// int count = 0;
+
+// foreach (string nums in numStr)
+// {
+//     if (int.TryParse(nums, out int number) && number > 0)
+//     {
+//         count ++;
+//     }
+// }
+
+// Console.WriteLine($"Kol-vo polozhitelnix chisel {count}");
+
+// Task2
+
+// Console.WriteLine("Vvedite znachenie dlya 1 pryamoi");
+
+// Console.Write("k1: ");
+// double k1 = double.Parse(Console.ReadLine());
+// Console.Write("b1: ");
+// double b1 = double.Parse(Console.ReadLine());
+
+// Console.WriteLine("Vvedite znachenie dlya 2 pryamoi");
+
+// Console.Write("k2: ");
+// double k2 = double.Parse(Console.ReadLine());
+// Console.Write("b2: ");
+// double b2 = double.Parse(Console.ReadLine());
+
+// if (k1 != k2)
+// {
+//     double X = (b2 - b1)/(k1-k2);
+//     double Y = k1 * X + b1;
+
+//     Console.WriteLine($"Tochka peresecheniya {X} , {Y}");
+// }
+// else
+// {
+//     Console.Write("Nevernaya sistema");
+// }
+
